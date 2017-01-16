2499d2f3aa7b47759866e260baa03f175f535898
CORP\bsurzynski <bsurzynski@infusion.com>
2017-01-16 13:28:10 +0100

    fix for enum

--- GameData/Models/GameType.cs
+++ GameData/Models/GameType.cs
@@ -3,9 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GraphQL.Types;
 
 namespace GameData.Models
 {
+    public class GameTypeEnum : EnumerationGraphType
+    {
+        public GameTypeEnum()
+        {
+            Name = "Game Type";
+            Description = "Test Desc";
+            AddValue("Simulations" , "Simulations",0);
+            AddValue("Action", "Action", 1);
+            AddValue("Sports", "Sports", 2);
+            AddValue("RPG", "RPG", 3);
+            AddValue("FPS", "FPS", 4);
+        }
+    }
     public enum GameType
     {
         Simulations,
--- GameData/Types/GameTypeQL.cs
+++ GameData/Types/GameTypeQL.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Xml.Schema;
 using GameData.Models;
 using GraphQL.Types;
+using GraphQLParser.AST;
 
 namespace GameData.Types
 {
@@ -15,8 +16,9 @@ namespace GameData.Types
         {
             Field(x => x.Id).Description("Id of game");
             Field(x => x.Name,true).Description("Name of game");
-            //Field(x => x.GameType, true).Description("Game Type");
-            Field(x => x.GameType.ToList()).Description("Description");
+            Field<ListGraphType<GameTypeEnum>>("GameType");
+            //Field<ListGraphType<GameTypeQL>>("friends");
+
             Field(x => x.Year, true).Description("Year");
         }
     }
